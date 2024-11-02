387ff09553af937660e32050b10eabe72903288f
aka <akadev8@outlook.com>
2024-11-02 20:36:23 +0100

    Update Header.js
    Signed-off-by: aka <akadev8@outlook.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
-// src/components/Dashboard.js
+// src/components/Header.js
 import React from 'react';
 
-function Dashboard() {
+function Header() {
   return (
-    <div>
-      <h2>Dashboard</h2>
-      <p>Welcome to the AiBlender Dashboard!</p>
-    </div>
+    <header>
+      <h1>AiBlender</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/dashboard">Dashboard</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
-export default Dashboard;
+export default Header;
