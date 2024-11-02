d44cf6d5ee24e43cfa38cd6e7f874231a0240232
aka <akadev8@outlook.com>
2024-11-02 20:31:00 +0100

    Update App.js
    Signed-off-by: aka <akadev8@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+
 // src/App.js
 import React from 'react';
 import Header from './components/Header';
+import Dashboard from './components/Dashboard';
+import { analyzeImage } from './models/image';
 import { analyzeText } from './models/nlp';
+import { analyzeSentiment } from './models/sentiment';
 
 function App() {
   return (
     <div>
       <Header />
+      <Dashboard />
       <h1>Welcome to AiBlender!</h1>
       <p>{analyzeText('Hello, AiBlender!')}</p>
+      <p>{analyzeSentiment('This is fantastic!')}</p>
     </div>
   );
 }
